c6251aae07d0feec8d79739be2614eea57404e49
OwenRamire <owenomar_ramirez@ucol.mx>
2020-08-05 19:47:38 -0500

    Add trending and recent recipes

---
+++ src/data/recipesRecent.js
@@ -0,0 +1,239 @@
+export var recipesRecent = [
+  {
+    name: 'Napolitan Pizza',
+    section: 'Recent',
+    image: 'URL',
+    servings: '2 servings',
+    ingredients: [
+      {
+        name: 'Peperoni',
+        quantity: '30 gr.',
+      },
+      {
+        name: 'Tomatoe Sauce',
+        quantity: '1/2 cup.',
+      },
+      {
+        name: 'Powder',
+        quantity: '500 gr.',
+      },
+      {
+        name: 'Yeast',
+        quantity: '40 gr.',
+      },
+    ],
+  },
+
+  {
+    name: 'Pesto Pasta',
+    section: 'Trending',
+    image: 'URL',
+    servings: '1 serving',
+    ingredients: [
+      {
+        name: 'Spagetti',
+        quantity: '100 gr.',
+      },
+      {
+        name: 'Albahaca',
+        quantity: '30 gr.',
+      },
+      {
+        name: 'Mushroms',
+        quantity: '50 gr.',
+      },
+      {
+        name: 'Garlic',
+        quantity: '2 dientes.',
+      },
+      {
+        name: 'Olive Oil',
+        quantity: '120 ml.',
+      },
+    ],
+  },
+
+  {
+    name: 'Cereal',
+    section: 'Recent',
+    image: 'URL',
+    servings: '1 servings',
+    ingredients: [
+      {
+        name: 'Cereal',
+        quantity: '200 gr.',
+      },
+      {
+        name: 'Milk',
+        quantity: '350 ml.',
+      },
+      {
+        name: 'Strawberries',
+        quantity: '50 gr.',
+      },
+    ],
+  },
+
+  {
+    name: 'Lemmon Pie',
+    section: 'Trending',
+    image: 'URL',
+    servings: '8 pieces',
+    ingredients: [
+      {
+        name: 'Cookies',
+        quantity: '2 cups',
+      },
+      {
+        name: 'Melted butter',
+        quantity: '1 taza.',
+      },
+      {
+        name: 'Cream cheese',
+        quantity: '1 cup',
+      },
+      {
+        name: 'Condesed milk',
+        quantity: '1 cup',
+      },
+      {
+        name: 'Evaporated milk',
+        quantity: '1 cup',
+      },
+      {
+        name: 'Lemon juice',
+        quantity: '1/4 cup',
+      },
+      {
+        name: 'Whiped cream',
+        quantity: '1/2 gr.',
+      },
+      {
+        name: 'Lemon zest',
+        quantity: '',
+      },
+    ],
+  },
+
+  {
+    name: 'Hotcakes',
+    section: 'Recent',
+    image: 'URL',
+    servings: '8 pieces',
+    ingredients: [
+      {
+        name: 'Eggs',
+        quantity: '1 piece',
+      },
+      {
+        name: 'Melted butter',
+        quantity: '100 gr.',
+      },
+      {
+        name: 'Powder',
+        quantity: '500 gr.',
+      },
+      {
+        name: 'Baking soda',
+        quantity: '20 gr.',
+      },
+    ],
+  },
+  {
+    name: 'Mexican Eggs',
+    section: 'Recent',
+    image: 'URL',
+    servings: '4 servings',
+    ingredients: [
+      {
+        name: 'Eggs',
+        quantity: '6 pieces',
+      },
+      {
+        name: 'Tomato',
+        quantity: '3 pieces',
+      },
+      {
+        name: 'Onion',
+        quantity: '1 piece',
+      },
+      {
+        name: 'Serrano chili',
+        quantity: '1 piece',
+      },
+      {
+        name: 'Salt',
+        quantity: '1 pinch',
+      },
+    ],
+  },
+  {
+    name: 'Classic Sandwich',
+    section: 'Trending',
+    image: 'URL',
+    servings: '1 serving',
+    ingredients: [
+      {
+        name: 'Sliced bread',
+        quantity: '2 pieces.',
+      },
+      {
+        name: 'Tomato',
+        quantity: '2 rebanadas',
+      },
+      {
+        name: 'Onion',
+        quantity: '2 slices',
+      },
+      {
+        name: 'Jalapeño',
+        quantity: '1 piece',
+      },
+      {
+        name: 'Ham',
+        quantity: '2 slices',
+      },
+      {
+        name: 'Queso amarillo',
+        quantity: '1 rebanada.',
+      },
+    ],
+  },
+
+  {
+    name: 'Salmon with Garlic',
+    section: 'Recent',
+    image: 'URL',
+    servings: '1 serving',
+    ingredients: [
+      {
+        name: 'Salmon',
+        quantity: '30 gr.',
+      },
+      {
+        name: 'Garlic',
+        quantity: '2 pieces.',
+      },
+      {
+        name: 'Rosmery',
+        quantity: '30 gr.',
+      },
+      {
+        name: 'Butter',
+        quantity: '3 spoons',
+      },
+      {
+        name: 'Olive oil',
+        quantity: '50 ml',
+      },
+      {
+        name: 'Salt',
+        quantity: 'al gusto',
+      },
+      {
+        name: 'Peper',
+        quantity: 'al gusto',
+      },
+    ],
+  },
+];
