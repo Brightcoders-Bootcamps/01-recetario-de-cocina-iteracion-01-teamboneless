b6731af64e5334a1e25f52e082987120d6373dd2
OwenRamire <owenomar_ramirez@ucol.mx>
2020-08-05 19:49:29 -0500

    Modify modal layout

--- src/components/ModalRecipe.component.js
+++ src/components/ModalRecipe.component.js
@@ -4,9 +4,13 @@ import {
   Modal,
   StyleSheet,
   Text,
-  TouchableHighlight,
+  ImageBackground,
   View,
+  ScrollView
 } from 'react-native';
+import IconsEvilIcon   from 'react-native-vector-icons/EvilIcons';
+import {Colors} from '../utils/themes';
+import {recipesRecent} from '../data/recipesRecent';
 
 const modal = ({visible, show, hide}) => {
   return (
@@ -20,57 +24,110 @@ const modal = ({visible, show, hide}) => {
         }}>
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <Text style={styles.modalText}>Hello World!</Text>
-
-            <TouchableHighlight
-              style={{...styles.openButton, backgroundColor: '#2196F3'}}
-              onPress={hide}>
-              <Text style={styles.textStyle}>Hide Modal</Text>
-            </TouchableHighlight>
+            <ImageBackground
+              source={require('./../assets/pizza.jpg')}
+              style={{flex: 0.7}}
+              imageStyle={{opacity: 0.6}}>
+              <View
+                style={{
+                  flex: 0.1,
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  paddingHorizontal: 25,
+                  marginTop: 15,
+                }}>
+                <View style={{flex: 1, marginVertical: 10}}>
+                  <IconsEvilIcon
+                    onPress={hide}
+                    name="close"
+                    size={40}
+                    color="white"
+                    fontWeight={2}
+                  />
+                </View>
+                <View style={{display: 'flex', flexDirection: 'row'}}>
+                  <IconsEvilIcon
+                    name="share-apple"
+                    size={40}
+                    color="white"
+                    style={{marginRight: 15}}
+                  />
+                  <IconsEvilIcon name="heart" size={40} color="white" />
+                </View>
+              </View>
+              <View style={{top: '65%', left: '5%'}}>
+                <Text style={styles.sectionName}>TRENDING</Text>
+                <Text style={styles.recipeName}>Pizza</Text>
+              </View>
+            </ImageBackground>
+            <View style={{margin: 15}}> 
+              <View style={{borderWidth: 2,}}>
+                <Text style={styles.servingsRecipe}>
+                  Ingredients for XX servings 
+                </Text>
+              </View>
+            </View>
+            <View style={{ display: 'flex', flexDirection: 'row', margin: 15,}}>
+              <View style={{flex: 8}}>
+                <Text style={styles.Ingredients }>{recipesRecent[0].ingredients[0].name}</Text>
+              </View>
+              <View style={{flex:4 }}>                
+                  <Text style={styles.Ingredients}>{recipesRecent[0].ingredients[0].quantity}</Text>
+              </View>
+            </View>
+            <View style={{ display: 'flex', flexDirection: 'row', margin: 15, }}>
+              <View style={{ flex: 8 }}>
+                <Text style={styles.Ingredients}>puff pastry</Text>
+              </View>
+              <View style={{ flex: 4 }}>
+                <Text style={styles.Ingredients}>1 sheet</Text>
+              </View>
+            </View>
+            <View style={{ display: 'flex', flexDirection: 'row', margin: 15, }}>
+              <View style={{ flex: 8 }}>
+                <Text style={styles.Ingredients}>puff pastry</Text>
+              </View>
+              <View style={{ flex: 4 }}>
+                <Text style={styles.Ingredients}>1 sheet</Text>
+              </View>
+            </View>
           </View>
         </View>
       </Modal>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
   },
   modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
+    flex: 1,
+    backgroundColor: Colors.backgroundScreen,
     shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
   },
-  openButton: {
-    backgroundColor: '#F194FF',
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
+  recipeName: {
+    color: Colors.white,
+    fontSize: 30,
+    opacity: 1,
+    fontWeight: "100",
   },
-  textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
+  sectionName: {
+    color: Colors.white,
+    fontSize: 15,
   },
-  modalText: {
-    marginBottom: 15,
-    textAlign: 'center',
+  servingsRecipe:{
+    color:Colors.white
   },
+  Ingredients:{
+    color: Colors.white, 
+    fontSize: 15,
+    paddingBottom: 15,
+    borderBottomWidth: 1.5,
+    borderBottomColor: '#323232',
+  }, 
 });
 
 export default modal;
