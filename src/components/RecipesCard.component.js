74b480a58dd8628a1533d217c3892d490a5d5ab6
Erik Parra <erikparrasantillan@gmail.com>
2020-08-12 18:30:39 -0500

    Implement props over the components

--- src/components/RecipesCard.component.js
+++ src/components/RecipesCard.component.js
@@ -2,20 +2,26 @@ import React from 'react';
 import {View, TouchableOpacity, Image, Text} from 'react-native';
 import {styles} from '../utils/styles';
 
-const recipiesCard = (props) => {
+const recipesCard = (props) => {
   const {
     data: {image, name},
-    testStyle: {stylesTest},
+    containerStyle,
+    imageStyle,
+    textStyle,
   } = props;
 
   return (
-    <View style={{stylesTest}}>
-      <TouchableOpacity style={{width: 135}}>
-        <Image style={styles.recipeImage} source={image} />
-        <Text style={styles.title}>{name}</Text>
+    <View style={[containerStyle]}>
+      <TouchableOpacity
+        style={[{width: 135}, containerStyle]}
+        onPress={() => {
+          console.log(props.data, 'bobi');
+        }}>
+        <Image style={[styles.recipeImage, imageStyle]} source={image} />
+        <Text style={[styles.title, textStyle]}>{name}</Text>
       </TouchableOpacity>
     </View>
   );
 };
 
-export default recipiesCard;
+export default recipesCard;
